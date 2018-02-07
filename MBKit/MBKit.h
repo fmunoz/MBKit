c6e7b3820e6adc364cfa631afa41cca0b1c0333a
Franklin Munoz <fmunoz@magicboxsoft.com>
2018-02-07 10:42:26 -0800

    Initial skeleton project

---
+++ MBKit/MBKit.h
@@ -0,0 +1,19 @@
+//
+//  MBKit.h
+//  MBKit
+//
+//  Created by Franklin Munoz on 2/7/18.
+//  Copyright © 2018 Magic Box Software Solutions LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MBKit.
+FOUNDATION_EXPORT double MBKitVersionNumber;
+
+//! Project version string for MBKit.
+FOUNDATION_EXPORT const unsigned char MBKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MBKit/PublicHeader.h>
+
+
