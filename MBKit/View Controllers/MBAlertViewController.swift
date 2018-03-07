f2f6fc5a9aeb1560504946f7e70c376ab5837c91
Franklin Munoz <fmunoz@magicboxsoft.com>
2018-03-06 18:35:43 -0800

    progress

---
+++ MBKit/View Controllers/MBAlertViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MBAlertViewController.swift
+//  MBKit
+//
+//  Created by Franklin Munoz on 2/15/18.
+//  Copyright © 2018 Magic Box Software Solutions LLC. All rights reserved.
+//
+
+import UIKit
+
+open class MBAlertViewController: UIViewController {
+
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
